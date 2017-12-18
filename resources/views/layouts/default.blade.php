9e2cba554d8e968706155a0ab436fad6643c0322
luojun <luojun@ssitcloud.com>
2017-12-18 18:26:44 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title','Sample APP')</title>
+    <title>@yield('title','Sample APP') - Laravel 基础教程</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+      @include('layouts._header')
+    <div class="container">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
   </body>
 </html>
