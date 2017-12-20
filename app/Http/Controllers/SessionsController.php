27150e72432c6c3b37744459ef72ea3af77698d7
luojun <luojun@ssitcloud.com>
2017-12-20 02:53:34 +0000

    Add account activations

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -25,23 +25,26 @@ class SessionsController extends Controller
 
     public function store(Request $request)
     {
-        $credentials = $this->validate($request,[
-            'email' => 'required|email|max:255',
-            'password' => 'required|min:6'
-        ]);
+       $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
+       ]);
 
-        if(Auth::attempt($credentials,$request->has('remember'))){
-            //登录成功时的操作
-            session()->flash('success','欢迎回来');
-            return redirect()->intended(route('users.show',[Auth::user()]));
-        }else {
-            # code...
-            session()->flash('danger','登录信息错误');
-            return redirect()->back();
-        }
-
-        return;
+       if (Auth::attempt($credentials, $request->has('remember'))) {
+           if(Auth::user()->activated) {
+               session()->flash('success', '欢迎回来！');
+               return redirect()->intended(route('users.show', [Auth::user()]));
+           } else {
+               Auth::logout();
+               session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+               return redirect('/');
+           }
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back();
+       }
     }
+
 
     //destroy方法，退出登录
     public function destroy(){
--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::resource('users','UsersController');
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+Route::get('signup/comfirm/{token}','UsersController@confirmEmail')->name('confirm_email');
