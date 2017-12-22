c7f6b07b3b988a4ceb1618fbbabbe5e89ea6e631
luojun <luojun@ssitcloud.com>
2017-12-22 01:02:21 +0000

    Fixed config\app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://sample.test'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
